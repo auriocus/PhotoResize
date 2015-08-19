eea0b42bd6620777abec544da75c277258f4e93a
Christian daheim <auriocus@gmx.de>
2015-08-19 21:00:31 +0200

    Commit of a version working in both WIndows 32/64 and OSX

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PhotoResize
 A single-purpose extension for Tcl to resize/resample photo images
+
+Use the standard ./configure; make; make install incantation to build this package. Tested on OSX and Windows/MinGW
---
+++ generic/photoresize.hpp
@@ -0,0 +1,72 @@
+#ifndef PHOTORESIZE_HPP
+#define PHOTORESIZE_HPP
+
+
+#ifdef SWIG
+%module photoresize
+%{
+#undef SWIG_version
+#define SWIG_version "0.1"
+%}
+
+%init{
+  #ifdef USE_TK_STUBS
+  if (Tk_InitStubs(interp, (char*)"8.1", 0) == NULL) {
+    return TCL_ERROR;
+  }
+  #endif  
+
+  //Tcl_PkgRequire(interp, "Tk", "", false);
+}
+%include exception.i
+%include typemaps.i
+%include std_string.i
+%{
+#include "photoresize.hpp"
+%}
+%typemap(in) Tk_PhotoHandle {
+  $1 = Tk_FindPhoto (interp, Tcl_GetString($input));
+  if ($1==NULL) {
+    SWIG_exception(SWIG_RuntimeError, "Photo not found");
+  }
+}
+
+%typecheck(SWIG_TYPECHECK_POINTER) Tk_PhotoHandle {
+  $1 = Tk_FindPhoto (interp, Tcl_GetString($input))!=NULL;
+}  
+
+%exception {
+  try {
+    $function
+  } catch (const std::string &msg) {
+    SWIG_exception(SWIG_RuntimeError, const_cast<char*>(msg.c_str()));
+  } catch (...) {
+    SWIG_exception(SWIG_RuntimeError, "Some C++-Error");
+  }
+}
+
+#else 
+ // C-preprocessor
+#include <tcl.h>
+#include <tk.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+//#include <malloc.h>
+#include <string>
+#include <sstream>
+#include <vector>
+
+#define STHROW(msg) { \
+	 std::ostringstream err;\
+	 err<<msg; \
+	 throw err.str(); }
+
+
+
+#endif //!SWIG
+
+std::string resizephoto(Tcl_Interp *interp, Tk_PhotoHandle source, Tk_PhotoHandle target, int xsize, int ysize);
+#endif
